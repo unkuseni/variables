3c8b398816ab50ba6bc1cf8537f4a1a96e41eba9
unkuseni <amudalatadeoye@gmail.com>
2023-08-05 18:01:33 +0100

    learning about references

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,8 @@
+// Lets create a dangling reference
 fn main() {
-       let original_string = String::from("Hello, World!");
-   
-       let transferred_string = transfer_ownership(original_string);
-   
-       // The original_string is no longer valid here, as ownership has been transferred
-       // to transferred_string
-   
-       println!("Transferred string: {}", transferred_string);
-   }
-   
-   fn transfer_ownership(mut string: String) -> String {
-       string.push_str(" This is transferred chaos!");
-       string
-   }
+    let reference_to_nothing = dangle();
+}
+fn dangle() -> &String {
+    let s = String::from("hello");
+    &s
+}
