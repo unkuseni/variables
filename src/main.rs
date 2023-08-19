d0266684e2400d7b4d71fbe5a7a48d9fcee314a0
unkuseni <amudalatadeoye@gmail.com>
2023-08-19 23:45:08 +0100

    confirming reference mutability

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     // Create a vector with initial values
     let vec = vec![1, 2, 3, 4];
-    // prinit each value in the vector
+    // Increment and print each value in the vector
     for  element in &vec {
 let incr = *element + 1;
         println!("Increment was: {}", incr);
